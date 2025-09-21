39bcb21f87cb02f768f821208fe86aa6e5be1434
Oveiss <oveiss.ershadian@gmail.com>
2025-09-21 16:31:44 +0330

    .

--- src/components/eventCard/eventCard.tsx
+++ src/components/eventCard/eventCard.tsx
@@ -1,18 +1,24 @@
-import { IEvent } from '@/core/types/IEvent';
-import Image from 'next/image'
-import { FC } from 'react';
+import { IEvent } from "@/core/types/IEvent";
+import Image from "next/image";
+import { FC } from "react";
 
 interface IProps {
   event: IEvent;
 }
 const EventCard: FC<IProps> = ({ event }) => {
   return (
-    <div className="flex flex-col p-1.5 bg-emerald-400 p-0.5">
-        <Image src={event.imageAddress} alt='image' width={200} height={400}/>
-        <h3 className="">{event.title}</h3>
-        <p className="">{event.description}</p>
+    <div className="flex flex-col p-1.5 bg-emerald-400 ">
+      <Image
+        src={event.imageAddress}
+        alt={event.imageAddress}
+        width={200}
+        height={200}
+        className="bg-amber-200"
+      />
+      <h3 className="">{event.title}</h3>
+      <p className="">{event.description}</p>
     </div>
-  )
-}
+  );
+};
 
-export default EventCard
+export default EventCard;
